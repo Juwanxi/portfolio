999f33e9eb4381f7735eec017fd054465764ae63
Juwanxi <juwanrc@gmail.com>
2022-06-30 20:34:41 -0500

    replacing preact with svelte

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+	function handleClick() {
+	}
+</script>
+
+<button on:click={handleClick}>
+    
+</button>
+
+<style lang="scss">
+button {
+	display: inline-block;
+	border: 3px solid currentColor;
+	padding: 0.5em 1em;
+	font-weight: 700;
+	text-transform: uppercase;
+}
+
+
+</style>
---
+++ src/components/Footer/Footer.svelte
@@ -0,0 +1,17 @@
+<footer>
+    &copy; {new Date().getFullYear()} Juwan Cohen
+    
+</footer>
+
+<style lang="scss">
+
+footer{
+    text-align: center;
+	padding-top: 8rem;
+	padding-right: 2rem;
+	padding-bottom: 4rem;
+	padding-left: 2rem;
+}
+
+</style>
+
